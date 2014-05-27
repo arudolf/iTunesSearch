162fe91a7c97a6ee0b17077dc06f7573f2a00dc1
Ana Rudolf <anarudolf@Ana-Rudolfs-iMac.local>
2014-05-27 18:33:41 +0200

    AFNetworking added

--- iTunesSearch/ViewController.m
+++ iTunesSearch/ViewController.m
@@ -14,10 +14,14 @@
 
 @implementation ViewController
 
+#pragma mark - Initialization
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,43 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UISearchBarDelegate methods
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    NSLog(@"search clicked");
+    [table reloadData];
+}
+
+#pragma mark - UITableViewDelegate methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *MyIdentifier = @"SearchResult";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:MyIdentifier];
+    }
+    
+    cell.textLabel.text = @"My Text";
+    cell.detailTextLabel.text = @"opis";
+    return cell;
+}
+
 
 @end
