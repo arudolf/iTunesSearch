e7c55a64cfd7b76c2a42413cad4cae37ab019d06
Ana Rudolf <anarudolf@Ana-Rudolfs-iMac.local>
2014-05-27 17:56:56 +0200

    Initial Commit

---
+++ iTunesSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunesSearch
+//
+//  Created by Ana Rudolf on 27/05/14.
+//  Copyright (c) 2014 Ana Rudolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
